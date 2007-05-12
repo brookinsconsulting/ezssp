00e7011aeedadc86e7740b2436c1a21926a6c0e7
Kristof Coomans <kristof.coomans@telenet.be>
2007-05-12 08:35:53 +0000

    - extension info file

---
+++ extension/ezssp/ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+class ezsspInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "Subtree Skeleton Publisher",
+            'Version' => "0.x",
+            'Copyright' => "Copyright (C) 2007 Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
